27d7a19a328cbb5873481a186ab6b708e3d5e7c5
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-10-29 00:21:09 +0300

    Move create-docker-host.sh to scripts folder
    Use just one script for docker-host creation

--- scripts/create-docker-host.sh
+++ scripts/create-docker-host.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+#
+# For using this script, first export GOOGLE_PROJECT environment
+# variable with GCE project id, for example:
+# export GOOGLE_PROJECT=project-example
+#
 
-docker-machine create --driver google --google-project otus-infra \
+docker-machine create --driver google \
   --google-zone europe-west1-b \
-  --google-machine-type g1-small \
-  --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1604-xenial-v20171002 \
+  --google-machine-type n1-standard-1 \
+  --google-open-port 9090/tcp \
+  --google-open-port 9292/tcp \
+  --google-machine-image ubuntu-os-cloud/global/images/ubuntu-1604-xenial-v20171002 \
   docker-host
