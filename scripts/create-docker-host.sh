ae9b8b636256c0b28c9c544d4a3059513d9721c4
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-10-29 11:17:18 +0300

    Add grafana support

--- scripts/create-docker-host.sh
+++ scripts/create-docker-host.sh
@@ -10,5 +10,6 @@ docker-machine create --driver google \
   --google-machine-type n1-standard-1 \
   --google-open-port 9090/tcp \
   --google-open-port 9292/tcp \
+  --google-open-port 3000/tcp \
   --google-machine-image ubuntu-os-cloud/global/images/ubuntu-1604-xenial-v20171002 \
   docker-host
