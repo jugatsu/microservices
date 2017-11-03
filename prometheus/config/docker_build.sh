26f2ce71c6152fc1a2c038a311b5efd3a0822098
Anton Kvashenkin <anton.jugatsu@gmail.com>
2017-11-03 07:41:19 +0300

    Fix quotes in prometheus docker_build.sh

--- prometheus/config/docker_build.sh
+++ prometheus/config/docker_build.sh
@@ -6,7 +6,7 @@ IMG="$NAME:$TAG"
 LATEST="$NAME:latest"
 
 docker build --build-arg VCS_REF=`git rev-parse --abbrev-ref HEAD` \
-  --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` \
+  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
   -t $IMG .
 
 docker tag $IMG $LATEST
